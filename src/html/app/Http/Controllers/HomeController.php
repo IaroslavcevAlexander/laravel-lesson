f2632753cb1acf8c613256cd9d39924b7e8dea17
IaroslavcevAlexander <aroslavcevsasa@gmail.com>
2026-06-05 12:46:12 +0300

    18. Выполнение sql-запросов

--- src/html/app/Http/Controllers/HomeController.php
+++ src/html/app/Http/Controllers/HomeController.php
@@ -4,44 +4,47 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use app\Models\Post;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
 
     public function index()
     {
-//        for($i = 1; $i <= 16; $i++){
-//            Post::create([
-//                'title' => "Post $i",
-//                'slug' => "post-$i",
-//                'content' => "Post $i content",
-//            ]);
+//        $posts = DB::select("select * from posts where id > ?", [5]);
+//        $posts = DB::select("select * from posts where id > :id", ['id' => 5]);
+//        $posts = DB::scalar("select count(*) from posts");
+//        dump($posts);
+//        foreach ($posts as $post) {
+//            echo $post->title . '<br>';
 //        }
+//        $res = DB::insert("insert into posts (title, slug, content, created_at, updated_at) values(?,?,?,?,?)", ['Post 16', 'post 16','Post 16 content', now(), now()]);
+//        $res = DB::update("update posts set title = concat(title, ?), updated_at = ? where id = ?", ['(edited2)', now(), 16]);
+//        $res = DB::delete("delete from posts where id = ?", [16]);
+//        dump($res);
+//        $posts = Post::all();
+//        dump($posts);
+//        $posts = DB::table('posts')
+//            ->where('id', '>', 1)
+//            ->where('id', '<', 9)
+//            ->get(['id', 'title']);
 
-//        $category = Category::find(4);
-//        dump($category);
-//
-//
-//        foreach ($category->posts as $post) {
-//            dump($post->title);
-//        }
+//        $posts = DB::table('posts')->pluck('title', 'id');
+//        $posts = DB::table('posts')->count();
+//        $posts = DB::table('posts')->max('id');
+//        $posts = DB::table('posts')->min('id');
+        $posts = DB::table('posts')
+            ->select('posts.*', 'tags.title as tag_title')
+            ->join('tags', 'posts.id', '=', 'tags.id', 'left')
+//            ->join('tags', 'post_tag.tag_id', '=', 'tags.id')
+            ->orderBy('posts.id')
+            ->get();
 
-//        $post = Post::find(2);
-//        foreach ($post->categories as $category) {
-//            dump($category->title);
-//        }
 
-//        $post = Post::with('categories')->find(1);
-//        dump($post);
-
-        $post = Post::find(3);
-
-//        $post->categories()->attach([2, 4]);
-        $post-> categories()->detach(2);
+        dump($posts);
 
         $posts = Post::with('categories')->get();
 
-        dump($posts);
         return view('home', compact('posts'));
     }
 
---
+++ src/html/database/migrations/2026_06_05_091517_create_tags_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->timestamps();
+        });
+
+        Schema::create('post_tag', function (Blueprint $table) {
+            $table->unsignedBigInteger('post_id');
+            $table->unsignedBigInteger('tag_id');
+
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
+
+            $table->primary(['post_id', 'tag_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('post_tag'); // сначала pivot (есть FK на tags)
+        Schema::dropIfExists('tags');     // потом саму таблицу
+    }
+};
