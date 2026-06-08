80ae1b56df7acc6f803555eb20435a6e06cbc9e3
IaroslavcevAlexander <aroslavcevsasa@gmail.com>
2026-06-08 11:33:12 +0300

    21. Accessors and Mutators (Аксессоры и Мутаторы)

--- src/html/app/Http/Controllers/HomeController.php
+++ src/html/app/Http/Controllers/HomeController.php
@@ -3,14 +3,30 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
-use app\Models\Post;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class HomeController extends Controller
 {
 
     public function index()
     {
+
+        User::create([
+            'name' => 'юрии',
+            'last_name' => 'цезарь',
+            'email' => 'Caesar@Mail.com',
+            'password' => '123',
+        ]);
+
+        $users = User::all();
+
+        foreach ($users as $user) {
+            echo "{$user->full_name} ! <br>";
+            echo"{$user->name} {$user->last_name} {$user->email} <br>";
+        }
 
         $posts = Post::with('category')->paginate(3)->withQueryString();
 
--- src/html/app/Models/User.php
+++ src/html/app/Models/User.php
@@ -6,11 +6,12 @@ namespace App\Models;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['name', 'last_name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
@@ -22,6 +23,47 @@ class User extends Authenticatable
      *
      * @return array<string, string>
      */
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected function lastName(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => ucfirst($value),
+            set: fn (string $value) => mb_convert_case($value, MB_CASE_TITLE, "UTF-8"),
+        );
+    }
+
+    protected function name(): Attribute
+    {
+        return Attribute::make(
+            get: fn(string $value) => ucfirst($value),
+            set: fn (string $value) => mb_convert_case($value, MB_CASE_TITLE, "UTF-8"),
+        );
+    }
+
+    protected function email(): Attribute
+    {
+        return Attribute::make(
+            get: fn(string $value) => strtolower($value),
+            set: fn(string $value) => strtolower($value),
+        );
+    }
+
+    protected function fullName(): Attribute
+    {
+        return Attribute::make(
+            get: function (mixed $value, array $attributes) {
+                $name = ucfirst($attributes['name']);
+                $last_name = ucfirst($attributes['last_name']);
+                return "{$name} {$last_name}";
+            },
+        );
+    }
+
     protected function casts(): array
     {
         return [
@@ -29,4 +71,6 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+
 }
